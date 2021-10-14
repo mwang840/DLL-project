52c18135b93b1425a44262c685b8c4de57b723d7
mwang840 <90732327+mwang840@users.noreply.github.com>
2021-10-13 22:01:09 -0400

    Update DLL.cpp

--- DLL.cpp
+++ DLL.cpp
@@ -1,4 +1,3 @@
-
 #include "DNode.hpp"
 #include "DLL.hpp"
 #include <iostream>
@@ -25,7 +24,7 @@ using namespace std;
 		last = node;
 		node->next = NULL;
 	}
-	
+
 	//Prints out the list in each node
 	void DLL::printList(){
 		if(first == NULL){
@@ -41,46 +40,105 @@ using namespace std;
 		}
 	}
 
-
 	//Removes the song title from the list if any exist
+	//Returns the index of the title being removed
 	int DLL::remove(string t){
-		int removed = 0;
-		Song *title = new Song();
+		int index = 0;
+		Song *s = new Song();
+		DNode *current = new DNode();
 		Song *next = new Song();
-		if(title->title !=NULL){
-			removed = -1;
+		DNode *temp = new DNode();
+		current = first;
+		if(first->song->title == t){
+			temp = first;
+			first = first->next;
+			cout<<"Removing";
+			first->song->printSong();
+			delete first;
+			numSongs--;
+			index = 0;
 		}
-		while(title->title != NULL){
-			removed++;
-			title = title->*next;
+		while(current->next != NULL){
+			if(current->song->title == t && (current == last)){
+				pop();
+			}
+			index++;
+			current = current->next;
 		}
-		return removed;
+		return index;
 	}
 
+	//Pops the last node off the list, will return the last object of the node
 	Song *DLL::pop(){
-		DNode *head = new DLL(); 
-		DNode *next = new DLL(); 
-		DNode *last = new DLL();
-		if(head == NULL){
+		Song *next = new Song();
+		Song *object = last->song;
+
+		if(first == NULL){
 			return NULL;
 		}
 
-		else if(head->next == NULL){ =
+		else if(first->next == NULL){
 			return NULL;
 		}
 		else{
-			DNode *temp = head; 
-			while(temp->next != NULL){
-				temp = temp->next;
-			}
-			DNode *temp = last;
-			Song *obj = temp->next;
-			last = last->prev;
-			delete temp;
-			last->next = NULL;
-			return *obj;
+			last = last->next;
+			delete last;
+			return object;
 		}
 
 	}
 
+	//// moves song with title s up one in the playlist.
+    //If it is at the beginning of the list,
+    //it will be moved to the end of the list.
+	void DLL::moveUp(string t){
+		DNode *current = new DNode();
+		current = first;
+		if(first->song->title == t){
+			last = first;
+		}
+		while(current != NULL){
+			current = current->next;
+		}
+
+		printList();
+	}
+
+	//moves song with title s down one in the playlist.
+	//If it is at the end of the list, it will move to
+	//beginning of the list.
+	void DLL::moveDown(string t){
+		DNode *current = new DNode();
+		current = last;
+		if(last->song->title == t){
+			first = last;
+		}
+		while(current != NULL){
+			current = current->next;
+		}
+	}
+
+	// gets the total list duration in minutes (passed in as pointers)
+	// and seconds (again, passed in as pointers)
+	void DLL::listDuration(int *tm, int *ts){
+
+	}
+
+	//
+	void DLL::makeRandom(){
+
+	}
+
+	//Destructor for dll
+	DLL::~DLL(){
+		cout<<"deleting pointers..."<<endl;
+		DNode *current  = first;
+		DNode *temp;
+		while(current != NULL){
+			temp = current->next;
+			delete current;
+			current = temp;
+		}
+		cout<<"List is deleted"<<endl;
+	}
 
